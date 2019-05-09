b20caccef85acf0f230c6e311526539c4deec099
shilpa <paishilpa4@gmail.com>
2019-05-09 22:29:07 +0530

    Commiting Rest AssuredOne

--- src/test/java/GetRequestTest/ValidateResponseCodeTest.java
+++ src/test/java/GetRequestTest/ValidateResponseCodeTest.java
@@ -1,69 +1,108 @@
 package GetRequestTest;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import jdk.internal.jline.internal.TestAccessible;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ValidateResponseCodeTest {
 
+    /*
+      @Test
+      public void ValidateResponseCodeSingleUser() {
+
+          Response response = RestAssured.get("https://reqres.in/api/users/2");
+          System.out.println(response.getStatusCode());
+          String body;
+
+          Assert.assertTrue(response.getStatusCode() == 200);
+          System.out.println(response.getStatusCode());
+
+          body = response.getBody().asString();
+          System.out.println(body);
+
+          JsonPath jp = new JsonPath(body);
+          String firstName = jp.getString("data.first_name");
+          System.out.println(firstName);
+
+
+      }
+      */
+    /*
+    @Test
+    public void validateMultipleUser200() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        String body;
+
+        Assert.assertTrue(response.getStatusCode() == 200);
+        System.out.println(response.getStatusCode());
+
+        body = response.getBody().asString();
+        System.out.println(body);
+
+        JsonPath jp = new JsonPath(body);
+
+        String firstName1 = jp.getString("data[0]");
+        //System.out.println(firstName1);
+        String firstName2 = jp.getString("data[1]");
+        // System.out.println(firstName2);
+
+        String firstName3 = jp.getString("data");
+        System.out.println(firstName3);
+
+    }   */
 
     @Test
-    public void ValidateResponseCode()
-    {
+    public void ValidateResponse() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 
-        Response response= RestAssured.get("https://reqres.in/api/users?page=4");
-        System.out.println(response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        //System.out.println(response.getBody().prettyPrint());
+        Response body = (Response) response.getBody();
+        List<Map<String, ?>> ExpectedResult = new ArrayList<>();
+        List<Map<String, ?>> actualData = response.jsonPath().getList("data");
 
-        Assert.assertTrue(response.getStatusCode()==200);
-        System.out.println(response.getStatusCode());
-        //validateData(response);
-        response.prettyPrint();
-        // System.out.println("Header :");
-        System.out.println("Headers are : "+response.headers());
-        System.out.println("JsonPath : "+response.jsonPath());
-        System.out.println("StatusLine :" +response.getStatusLine());
-        //Assert
-        List<Map<String, ?>>actualData= response.jsonPath().getList("data");
-        List<Map<String, ?>>expectedDataList;
-        /*Map map= new HashMap();
-        map.put("id",4);
-        map.put("first_name","Eve");
-        map.put("last_name",  "Holt");*/
+        Map<String, String> map1 = new HashMap<>();
+        Map<String, String> map2 = new HashMap<>();
+        Map<String, String> map3 = new HashMap<>();
 
+        map1.put("id", "4");
+        map1.put("first_name", "Eve");
+        map1.put("last_name", "Holt");
 
+        map2.put("id", "5");
+        map2.put("first_name", "Charles");
+        map2.put("last_name", "Morris");
 
+        map3.put("id", "6");
+        map3.put("first_name", "Tracey");
+        map3.put("last_name", "Ramos");
 
-        int dataSize= actualData.size();
-        System.out.println("Number of records: +dataSize");
-        for(int i=0;i<dataSize;i++)
-        {
-            System.out.println("Id is:" +actualData.get(i).get("id").toString());
-            System.out.println("Name : " +actualData.get(i).get("first_name").toString());
+        ExpectedResult.add(map1);
+        ExpectedResult.add(map2);
+        ExpectedResult.add(map3);
+
+        int datasize = actualData.size();
+
+        System.out.println(datasize);
+        for (int i = 0; i < datasize; i++) {
+            System.out.println("Id :" + actualData.get(i).get("id").toString());
+            System.out.println("First Name :" + actualData.get(i).get("first_name").toString());
+              System.out.println("Last Name :" + actualData.get(i).get("last_name").toString());
+            Assert.assertEquals(ExpectedResult.get(i).get("id").toString(), actualData.get(i).get("id").toString());
+
+            Assert.assertEquals(ExpectedResult.get(i).get("first_name").toString(), actualData.get(i).get("first_name").toString());
+            Assert.assertEquals(ExpectedResult.get(i).get("last_name").toString(), actualData.get(i).get("last_name").toString());
 
         }
-
-        Assert.assertEquals("10" , actualData.get(0).get("id").toString());
-        System.out.println(" DIplaying");
-
-
-
-
-            //System.out.println(" Number of Records: "+)
-
-
-
-
-        //Assert.assertTrue();
-
-
+        Assert.assertEquals("Eve", actualData.get(0).get("first_name").toString());
 
     }
-
-
 }
--- src/test/java/PostRequestTest/ValidateCreateRequest.java
+++ src/test/java/PostRequestTest/ValidateCreateRequest.java
@@ -1,21 +1,111 @@
 package PostRequestTest;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import jdk.internal.jline.internal.TestAccessible;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 public class ValidateCreateRequest {
 
-    @Test
-    public void ValidateResponseCode()
-    {
+   @Test(priority = 0)
+    public void validateRegisterInvalidUser() {
 
-        Response response= RestAssured.post("https://reqres.in/api/users");
-        //System.out.println(response.getStatusCode());
+        // Response resrequest= RestAssured.post("https://reqres.in/api/users");
+        //request.header("Content - Type", "Application/json");
+        RequestSpecification request = RestAssured.given();
+        request.header("Content Type", "Application/json");
+        JSONObject requestJson = new JSONObject();
+        try {
+            requestJson.put("email", "smp@gmail.com");
+            requestJson.put("password", "pistol");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        request.body(request.toString());
+        Response responseRegister = request.post("https://reqres.in/api/users");
+        System.out.println("Response Body" + responseRegister.getBody().asString());
+        Assert.assertEquals(responseRegister.getStatusCode(), 201);
+
+
+    }
 
 
 
+    @Test(priority = 1)
+    public void validateRegistervalidUser() {
 
+        // Response resrequest= RestAssured.post("https://reqres.in/api/users");
+        //request.header("Content - Type", "Application/json");
+        RequestSpecification request = RestAssured.given();
+        request.header("Content Type", "Application/json");
+        JSONObject requestJson = new JSONObject();
+        try {
+            requestJson.put("email", "");
+            requestJson.put("password", "");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        request.body(request.toString());
+        Response responseRegister = request.post("https://reqres.in/api/users");
+        System.out.println("Response Body" + responseRegister.getBody().asString());
+        Assert.assertEquals(responseRegister.getStatusCode(), 201);
+
+
+    }
+
+    @Test(priority = 2)
+    public void LoginSuccessful() {
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content Type", "Application/json");
+        JSONObject requestJson = new JSONObject();
+        try {
+            requestJson.put("email", "peter@klaven");
+            requestJson.put("password", "cityslicka");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        request.contentType("Application/json").body(requestJson.toString());
+
+        Response responseRegister = request.post("https://reqres.in/api/login");
+        System.out.println("Response Body" + responseRegister.getBody().asString());
+        Assert.assertEquals(responseRegister.getStatusCode(), 200);
+        if (responseRegister.getBody().asString() != null) {
+
+            System.out.println("LOGIN SUCCESSFUL !!!");
+        }
+
+    }
+        @Test(priority = 3)
+        public void LoginUnSuccessful(){
+
+            RequestSpecification request = RestAssured.given();
+            request.header("Content Type", "Application/json");
+            JSONObject requestJson = new JSONObject();
+            try {
+                requestJson.put("email", "peter@klaven");
+                //requestJson.put("password", "cityslicka");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            request.contentType("Application/json").body(requestJson.toString());
+
+            Response responseRegister = request.post("https://reqres.in/api/login");
+            System.out.println("Response Body" + responseRegister.getBody().asString());
+            String body=responseRegister.getBody().asString();
+            JsonPath jp = new JsonPath(body);
+            String errorMessage=jp.getString("error");
+            if(errorMessage.equals("Missing password")){
+                System.out.println("LOGIN UNSUCCESSFUL !!!");
+            }
+            Assert.assertEquals(responseRegister.getStatusCode(), 400);
 
 
 
@@ -23,6 +113,9 @@ public class ValidateCreateRequest {
 
 
 }
+
+
+
 
 
 
